2550a5b5f2b4206659aa6ded4cc834a8c8ccf70f
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-09-05 11:01:27 +0200

    sets express template with ts, lint and jest

---
+++ node/index.ts
@@ -0,0 +1,22 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+export const getMessage = () => {
+  const message: string = "Hello you"
+  return message;
+}
+
+console.log(getMessage());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
---
+++ node/test/unit/index.spec.ts
@@ -0,0 +1,5 @@
+describe('Check Jest', () => {
+  it('should run tests in node', () => {
+    expect(true).toBe(true)
+  })
+})
