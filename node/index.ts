ca8c7288961bd61a2172271beea8f34ab935410e
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-09-11 16:33:22 +0200

    adds mockData.json, connects to tmdb api, adds notes on the future structure of graphql api

--- node/index.ts
+++ node/index.ts
@@ -1,21 +1,47 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import fetch from 'node-fetch';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-export const getMessage = () => {
-  const message: string = "Hello you"
-  return message;
-}
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.APIReadAccessToken}`,
+  },
+};
 
-console.log(getMessage());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+app.get("/",  (req: Request, res: Response) => {
+  res.send("Czesc mordeczko")
+})
+
+app.get("/getMovies", (req: Request, res: Response) => {
+  const url = "https://api.themoviedb.org/3/movie/upcoming";
+
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((json) => console.log(json))
+    .catch((err) => console.error("error:" + err));
+
+  res.send("Express + TypeScript Server");
 });
+
+app.get("/getMovieVideo", (req: Request, res: Response) => {
+  const url = "http://api.themoviedb.org/3/movie/346698&append_to_response=videos";
+
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((json) => console.log(json))
+    .catch((err) => console.error("error:" + err));
+
+  res.send("Express + TypeScript Server");
+});
+
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
