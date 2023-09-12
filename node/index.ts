8548feb96cee4dc51b5677da775be6636a29c163
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-09-12 10:52:26 +0200

    adds cors

--- node/index.ts
+++ node/index.ts
@@ -2,19 +2,19 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import { graphqlHTTP } from "express-graphql";
 const schema = require("./src/schemas/index");
+const cors = require('cors');
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use(cors());
 app.get("/",  (req: Request, res: Response) => {
   res.send("Czesc mordeczko")
-})
+});
 
-app.use(
-  "/graphql",
-  graphqlHTTP({
+app.use( "/graphql", graphqlHTTP({
     schema,
     graphiql: true,
   })
