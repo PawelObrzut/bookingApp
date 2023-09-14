095450ffa37dd9c1843da9a427d60f7071291b58
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-09-15 00:52:26 +0200

    gets infinite carousel to work

--- client/src/components/Carousel/Carousel.tsx
+++ client/src/components/Carousel/Carousel.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loadMovies } from "../../features/moviesSlice";
 import { RootState } from "../../app/store";
@@ -6,6 +6,7 @@ import { AnyAction, ThunkDispatch } from "@reduxjs/toolkit";
 import classes from "./Carousel.module.scss";
 import { IMovie } from "../../types/types";
 import { PiPlayCircleThin } from "react-icons/pi";
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 
 interface ICarouselCardProps {
   movie: IMovie
@@ -17,13 +18,15 @@ const CarouselCard = ({ movie }: ICarouselCardProps) => {
       <div className={classes.movie}>
         <img className={classes.movie__image} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
         <div className={classes.movie__tag}>Premiere</div>
-        <div className={`${classes.movie__details}`}>
-          <span className={`btn ${classes.movie__details__span}`}>Details</span>
+
+        <div className={classes.movie__bg}>
+          <div className={`${classes.movie__details}`}>
+            <span className={`btn ${classes.movie__details__span}`}>Details</span>
+          </div>
+          <div className={classes.movie__trailer}>
+            <PiPlayCircleThin size={60} />
+          </div>
         </div>
-        <div className={classes.movie__trailer}>
-          <PiPlayCircleThin size={50} />
-        </div>
-        <div className={classes.movie__bg}></div>
       </div>
       <h1 className={classes.movie__title}>{movie.title}</h1>
     </li>
@@ -43,6 +46,24 @@ const Carousel = () => {
     }
   }, [status, dispatch]);
 
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleShiftLeft = () => {
+    // console.log(movies.slice(-1).concat(movies.slice(0, movies.length - 1)));
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? movies.length - 1 : prevIndex - 1
+    );
+  }
+
+  const handleShiftRight = () => {
+    // console.log(movies.slice(1, movies.length).concat(movies.slice(0, 1)));
+    setCurrentIndex((prevIndex) =>
+      prevIndex === movies.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const looppedMovies = [...movies.slice(currentIndex), ...movies.slice(0, currentIndex)];
+
   if (status === "loading") {
     return <div>Loading...</div>;
   } else if (status === "failed") {
@@ -51,9 +72,18 @@ const Carousel = () => {
 
   return (
     <div className={classes.carousel}>
+      <div className={classes.carousel__prev} onClick={handleShiftLeft}>
+        <BsChevronLeft size={50} />
+      </div>
       <ul className={`${classes.carousel__content} `}>
-        {movies?.map((movie: IMovie) => <CarouselCard key={movie.id} movie={movie} />)}
+        {looppedMovies
+          ?.map((movie: IMovie) => (
+            <CarouselCard key={movie.id} movie={movie} />
+          ))}
       </ul>
+      <div className={classes.carousel__next} onClick={handleShiftRight}>
+        <BsChevronRight size={50} />
+      </div>
     </div>
   )
 }
