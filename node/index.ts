73f169425dd76587f4c57605e1f8d9890dca6e2b
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-09-26 15:01:23 +0200

    creates few routes to fetch from tmdb in order to quickly create db content

--- node/index.ts
+++ node/index.ts
@@ -1,10 +1,10 @@
-import { ApolloServer } from 'apollo-server-express';
-import express, { Express, Request, Response } from "express";
+import { ApolloServer } from "apollo-server-express";
+import express, { Express, Response } from "express";
 
 import dotenv from "dotenv";
 dotenv.config();
 const port = process.env.PORT;
-const cors = require('cors');
+const cors = require("cors");
 
 import { typeDefs } from "./src/schemas/TypeDefs";
 import { resolvers } from "./src/schemas/Resolvers";
@@ -17,6 +17,47 @@ async function startApolloServer() {
   await server.start();
   server.applyMiddleware({ app });
 }
+
+console.log("ENV: ", process.env.PORT)
+
+app.get("/movies/:title", (req, res: Response) => {
+  const title = req.url.slice(8)
+
+  const url = `https://api.themoviedb.org/3/search/movie?query=${title}`;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.APIReadAccessToken}`,
+    },
+  };
+
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((json) => {
+      console.log('success')
+      res.json(json)
+    })
+    .catch((err) => console.error("error:" + err));
+});
+
+app.get("/movies/video/:id", (req, res: Response) => {
+  const id = req.url.slice(14)
+  const url = `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`;
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${process.env.APIReadAccessToken}`,
+    }
+  };
+  
+  fetch(url, options)
+    .then(res => res.json())
+    .then(json => res.json(json))
+    .catch(err => console.error('error:' + err));
+
+});
 
 startApolloServer().then(() => {
   app.listen(port, () => {
