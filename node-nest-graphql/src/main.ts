a246771a998710cf9979cc6a0eb3a53704737398
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-10-10 12:31:06 +0200

    removes BaseExceptionFilter middleware

--- node-nest-graphql/src/main.ts
+++ node-nest-graphql/src/main.ts
@@ -1,4 +1,4 @@
-import { BaseExceptionFilter, NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
@@ -6,7 +6,6 @@ const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new BaseExceptionFilter());
   app.enableCors();
   await app.listen(port);
   Logger.log(`🚀 Server running on http://localhost:${port}`, 'Bootstrap');
